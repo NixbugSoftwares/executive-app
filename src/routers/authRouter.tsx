6cf2cdfc095a5e928530afa60d81187d5f1caba3
Al-mehaboob <almehaboobnixbug@gmail.com>
2025-01-21 16:27:49 +0530

    Created a file for no net work error , and created router for that .

--- src/routers/authRouter.tsx
+++ src/routers/authRouter.tsx
@@ -1,9 +1,10 @@
 import { Login } from "../screens/auth";
+import { Nonet } from "../common";
 import { RouteObject } from "react-router-dom";
 
 const authRoutes: RouteObject[] = [
-    { path: "/login", element: <Login /> },
+  { path: "/login", element: <Login /> },
   { path: "/nonetwork", element: <Nonet /> },
-  ];
+];
 
 export default authRoutes;
