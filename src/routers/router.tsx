18690980b090d493ced5a47afbb5fa64a485cbd2
Al-mehaboob <almehaboobnixbug@gmail.com>
2025-01-22 11:17:31 +0530

    In order to create new routing logic , commented current routing page .

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -1,14 +1,14 @@
-import { RouteObject } from "react-router-dom";
-import App from "../App";
-import { Home } from "../screens/homepage";
-import { Account } from "../screens/accountpage";
-import { BusStop } from "../screens/busstoppage";
-import { ExeRole } from "../screens/executiveRolepage";
-import { Operator } from "../screens/operatorpage";
-import { CRole } from "../screens/companyRolepage";
-import { BusRoute } from "../screens/busroutepage";
-import { Fare } from "../screens/farepage";
-import { Bus } from "../screens/buspage";
+// import { RouteObject } from "react-router-dom";
+// import App from "../App";
+// import { Home } from "../screens/homepage";
+// import { Account } from "../screens/accountpage";
+// import { BusStop } from "../screens/busstoppage";
+// import { ExeRole } from "../screens/executiveRolepage";
+// import { Operator } from "../screens/operatorpage";
+// import { CRole } from "../screens/companyRolepage";
+// import { BusRoute } from "../screens/busroutepage";
+// import { Fare } from "../screens/farepage";
+// import { Bus } from "../screens/buspage";
 
 const routes: RouteObject[] = [
   { path: "/", element: <App /> },
