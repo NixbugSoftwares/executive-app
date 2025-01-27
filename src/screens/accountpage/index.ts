4f34904bc570716ac4be29033045e85b5f852e1a
Al-mehaboob <almehaboobnixbug@gmail.com>
2025-01-27 15:37:01 +0530

    Set5 up the roter.

--- src/screens/accountpage/index.ts
+++ src/screens/accountpage/index.ts
@@ -1,4 +1,6 @@
+
 // src/components/screens/homepage/index.ts
 export { default as Account } from './Account';
 export { default as AccountCreationForm } from './AccountForm';
 export { default as AccountListingTable } from './AccountListingPage';
+
