a45c808bfff49ebeadb6468aa3c470fab4b1208b
Al-mehaboob <almehaboobnixbug@gmail.com>
2025-02-05 15:13:21 +0530

    styles in landmark page

--- src/screens/landmarkpage/LandMarkPage.tsx
+++ src/screens/landmarkpage/LandMarkPage.tsx
@@ -5,9 +5,20 @@ import LandmarkListing from './LandmarkListingPage';
 
 function LandMarkPage() {
   return (
-    <Box>
+    <Box
+    sx={{
+      display: "flex",
+      height: "100vh",
+      width: "100%",
+      scrollBehavior: "auto",
+    }}>
       <Sidebar/>
+      <Box
+      sx={{ width: "100%", p: 3 }}
+      >
       <LandmarkListing />
+      </Box>
+      
 
     </Box>
   )
