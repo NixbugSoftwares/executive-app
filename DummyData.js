72733dbd3a257886f6b8c96c001f337a2f033b54
Al-mehaboob <almehaboobnixbug@gmail.com>
2025-02-07 15:21:43 +0530

    created a new file for save dummy data we used in project.

---
+++ DummyData.js
@@ -0,0 +1,76 @@
+
+
+// ***********************************************************************************************Dummy Datas********************************************************************************
+
+
+
+// *****************************************************************************************Account*****************************************************************************************
+
+
+// const data: Account[] = [
+//     { id: 1309, fullName: "Rihab Shihan", username: "rihab", password: "rihab@123", gender: "Male", designation: "React Native", email: "rihab@example.com", phoneNumber: "123-456-7890", status: "Active", role: { roleName: "Admin", permissions: [" managerole", "managelandmark "] } },
+//     { id: 2572, fullName: "Ashin Joseph", username: "ashin", password: "ashin123", gender: "Female", designation: "Python", email: "ashin@example.com", phoneNumber: "987-654-3210", status: "Active", role: { roleName: "Admin", permissions: ["manageexecutive", "managelandmark", "managecompany"] } },
+//     { id: 3509, fullName: "Reshma", username: "reshma", password: "reshma@123", gender: "Female", designation: "Devops", email: "reshma@example.com", phoneNumber: "123-7890-456", status: "inactive", role: { roleName: "Admin", permissions: ["manageexecutive", "managelandmark"] } },
+//     { id: 4567, fullName: "Al Mehaboob", username: "mehaboob", password: "mehaboob@123", gender: "Male", designation: "React", email: "mehaboob@example.com", phoneNumber: "859-2805-065", status: "inactive", role: { roleName: "Admin", permissions: ["manageexecutive", "managerole", "managelandmark"] } },
+//     { id: 5098, fullName: "Thinkal VB", username: "thinkal ", password: "thinkal123", gender: "Male", designation: "Python", email: "thinkal@example.com", phoneNumber: "872-555-0978", status: "inactive", role: { roleName: "Admin", permissions: ["manageexecutive", "managerole", "managelandmark", "managecompany"] } },
+//     { id: 6287, fullName: "subin Raj", username: "subin ", password: "subin123", gender: "Male", designation: "React-Native", email: "subin@example.com", phoneNumber: "555-555-5555", status: "inactive", role: { roleName: "Admin", permissions: ["manageexecutive, managerole", "managelandmark", "managecompany"] } },
+//     { id: 7990, fullName: "Ganesh Parthan", username: "ganeshan ", password: "ganeshan123", gender: "Male", designation: "python", email: "ganeshan@example.com", phoneNumber: "7890-555-123", status: "inactive", role: { roleName: "Admin", permissions: ["manageexecutive", "managerole", "managelandmark", "managecompany"] } },
+//     { id: 8967, fullName: "Leomessi", username: "messi10", password: "messi@123", gender: "Male", designation: "RWF", email: "messi@example.com", phoneNumber: "123-456-7890", status: "Active", role: { roleName: "Admin", permissions: [" managerole", "managelandmark "] } },
+//     { id: 4565, fullName: "C.ronaldo", username: "cr7", password: "cr@123", gender: "Male", designation: "CF", email: "cr@example.com", phoneNumber: "987-654-3210", status: "Active", role: { roleName: "Admin", permissions: ["manageexecutive", "managelandmark", "managecompany"] } },
+//     { id: 9900, fullName: "Neymar", username: "neymar11", password: "neymar@123", gender: "Male", designation: "LWF", email: "ney@example.com", phoneNumber: "123-7890-456", status: "inactive", role: { roleName: "Admin", permissions: ["manageexecutive", "managelandmark"] } },
+//     { id: 1765, fullName: "D.Maradona", username: "maradona10", password: "maradona@123", gender: "Male", designation: "AMF", email: "mardona@example.com", phoneNumber: "859-2805-065", status: "inactive", role: { roleName: "Admin", permissions: ["manageexecutive", "managerole", "managelandmark"] } },
+//     { id: 6623, fullName: "J.cryuff", username: "cruyff", password: "cruyf@123", gender: "Male", designation: "SS", email: "cruyf@example.com", phoneNumber: "872-555-0978", status: "inactive", role: { roleName: "Admin", permissions: ["manageexecutive", "managerole", "managelandmark", "managecompany"] } },
+//     { id: 9009, fullName: "Z.Zidane", username: "zidane ", password: "zidane123", gender: "Male", designation: "AMF", email: "zidane@example.com", phoneNumber: "555-555-5555", status: "inactive", role: { roleName: "Admin", permissions: ["manageexecutive, managerole", "managelandmark", "managecompany"] } },
+//     { id: 2232, fullName: "Lamin Yamal", username: "yamal ", password: "yamal123", gender: "Male", designation: "RWF", email: "yamal@example.com", phoneNumber: "7890-555-123", status: "inactive", role: { roleName: "Admin", permissions: ["manageexecutive", "managerole", "managelandmark", "managecompany"] } },
+  
+//   ];
+
+
+
+
+
+
+
+
+//***************************************************************************************************executive Role Datas ***************************************************************************************
+
+
+
+// const data : Role[] = [
+//     { id: 1, Rolename: "Chief Technology Officer", manageexecutive: true, managerole: false, managelandmark: true, managecompany: true  },
+//     { id: 2, Rolename: "Chief Information Officer (CIO)", manageexecutive: false, managerole: false, managelandmark: false, managecompany: false  },
+//     { id: 3, Rolename: "Project Manager", manageexecutive: false, managerole: true, managelandmark: true, managecompany: false  },
+//     { id: 4, Rolename: "Product Manager", manageexecutive: true, managerole: false, managelandmark: false, managecompany: true  },
+//     { id: 5, Rolename: "Software Engineer", manageexecutive: false, managerole: false, managelandmark: true, managecompany: false  },
+//     { id: 6, Rolename: "Backend Developer", manageexecutive: true, managerole: true, managelandmark: false, managecompany: true  },
+//     { id: 11, Rolename: "QA Engineer / Test Engineer", manageexecutive: false, managerole: false, managelandmark: false, managecompany: true },
+//     { id: 21, Rolename: "Data Analyst", manageexecutive: false, managerole: false, managelandmark: false, managecompany: false  },
+//     { id: 31, Rolename: "Technical Support Engineer", manageexecutive: true, managerole: false, managelandmark: true, managecompany: false  },
+//     { id: 41, Rolename: "Mobile App Developer", manageexecutive: false, managerole: true, managelandmark: false, managecompany: false  },
+//     { id: 51, Rolename: "Engineering Manager", manageexecutive: false, managerole: true, managelandmark: true, managecompany: true },
+
+// ];
+
+
+
+
+
+//************************************************************************* landmmark Datas ******************************************************************************************
+
+
+
+// const data: Landmark[] = [
+//     { id: 123, name: "Landmark1", location: "location1", status: "Verified", importance: "low" },
+//     { id: 26546, name: "Landmark2", location: "location2", status: "Verifying", importance: "medium" },
+//     { id: 3354, name: "Landmark3", location: "location3", status: "Verifying", importance: "high" },
+//     { id: 187, name: "Landmark4", location: "location4", status: "Verified", importance: "low" },
+//     { id: 2534, name: "Landmark5", location: "location5", status: "Verifying", importance: "medium" },
+//     { id: 3255, name: "Landmark5", location: "location5", status: "Verifying", importance: "high" },
+//     { id: 15346, name: "Landmark6", location: "location6", status: "Verified", importance: "low" },
+//     { id: 9782, name: "Landmark7", location: "location7", status: "Verifying", importance: "medium" },
+//     { id: 3897, name: "Landmark8", location: "location8", status: "Verifying", importance: "high" },
+//   ];
+
+
+
+
