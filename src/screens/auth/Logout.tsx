7ef44b9aac84738f79c00ea36f007e8bbb8263e2
Al-mehaboob <almehaboobnixbug@gmail.com>
2025-02-12 10:33:22 +0530

    just Created a file for logout.

---
+++ src/screens/auth/Logout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from '@mui/material';
+const Logout: React.FC = () => {
+  return(
+    <Box>
+      <h1>Logout</h1>
+    </Box>
+
+  )
+  
+};
+
+export default Logout;
